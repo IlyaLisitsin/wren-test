bff32bb08a688e560d883c98459214227b3ae482
Savchuk Ilya <ilyas.sporichev@gmail.com>
2017-11-06 21:57:51 +0300

    layout fixes

--- app/main.js
+++ app/main.js
@@ -1,7 +1,8 @@
 $('.interactive-item-container').mouseover(function () {
     var curItem = $(this);
     curItem.find('.overlay').addClass('hidden');
-    curItem.find('.interactive-item-content').removeClass('hidden');
+    // curItem.find('.interactive-item-content').removeClass('hidden');
+    curItem.parent().next().addClass('zindexDecrease');
     // curItem.addClass('widthIncrease');
     // curItem.next().addClass('widthDecrease');
     // curItem.prev().addClass('widthDecrease');
@@ -11,7 +12,8 @@ $('.interactive-item-container').mouseover(function () {
 $('.interactive-item-container').mouseout(function () {
     var curItem = $(this);
     curItem.find('.overlay').removeClass('hidden');
-    curItem.find('.interactive-item-content').addClass('hidden');
+    // curItem.find('.interactive-item-content').addClass('hidden');
+    curItem.parent().next().removeClass('zindexDecrease');
     // curItem.removeClass('widthIncrease');
     // curItem.next().removeClass('widthDecrease');
     // curItem.prev().removeClass('widthDecrease');
