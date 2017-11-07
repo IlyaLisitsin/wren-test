40352814ea1b65eaf504279cead3504a951fa708
Savchuk Ilya <ilyas.sporichev@gmail.com>
2017-11-07 21:13:33 +0300

    functionality expanded

--- app/main.js
+++ app/main.js
@@ -1,21 +1,47 @@
-$('.interactive-item-container').mouseover(function () {
+$('.interactive-item-container').mouseover(itemMouseOverHandler);
+$('.interactive-item-container').mouseout(itemMouseOutHandler);
+
+function itemMouseOverHandler () {
     var curItem = $(this);
     curItem.find('.overlay').addClass('hidden');
-    // curItem.find('.interactive-item-content').removeClass('hidden');
     curItem.parent().next().addClass('zindexDecrease');
-    // curItem.addClass('widthIncrease');
-    // curItem.next().addClass('widthDecrease');
-    // curItem.prev().addClass('widthDecrease');
     curItem.addClass('itemExpanded')
-});
+}
 
-$('.interactive-item-container').mouseout(function () {
+function itemMouseOutHandler () {
     var curItem = $(this);
     curItem.find('.overlay').removeClass('hidden');
-    // curItem.find('.interactive-item-content').addClass('hidden');
     curItem.parent().next().removeClass('zindexDecrease');
-    // curItem.removeClass('widthIncrease');
-    // curItem.next().removeClass('widthDecrease');
-    // curItem.prev().removeClass('widthDecrease');
     curItem.removeClass('itemExpanded')
+}
+
+$('.read-more').click(function () {
+    var curItem = $(this).parent();
+    var item = $('.interactive-item-container');
+    var glContainer = curItem.parent();
+
+    item.off();
+    item.addClass('zindexDecrease');
+    item.removeClass('itemExpanded');
+    curItem.addClass('itemActive');
+    curItem.find('.close').removeClass('hidden');
+    glContainer.addClass('itemActiveContainer');
+    glContainer.nextAll().addClass('zindexDecrease');
+});
+
+$('.close').click(function () {
+    console.log('toot')
+    var curItem = $(this).parent();
+    var item = $('.interactive-item-container');
+    var glContainer = curItem.parent();
+
+    item.mouseover(itemMouseOverHandler);
+    item.mouseout(itemMouseOutHandler);
+
+    item.removeClass('zindexDecrease');
+    curItem.removeClass('itemActive');
+    curItem.find('.close').addClass('hidden');
+    glContainer.removeClass('itemActiveContainer');
+    glContainer.nextAll().removeClass('zindexDecrease');
+    glContainer.find(item).removeClass('itemExpanded');
 });
